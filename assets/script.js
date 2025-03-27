e46ea71bd102c4008b542bbb248cc2ff2ee11f6a
Antonio Lieto <Antonio.lieto1999@gmail.com>
2025-03-27 16:20:43 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+const numDiv = document.createElement("div");
+const TableFunction = function () {
+  const tombola = document.getElementById("tombola");
+  for (let i = 1; i <= 99; i++) {
+    const numDiv = document.createElement("div");
+    numDiv.classList.add("casella");
+    numDiv.innerText = i;
+    numDiv.dataset.numero = i;
+    tombola.appendChild(numDiv);
+  }
+};
+
+const numRand = function () {
+  const random = Math.floor(Math.random() * 99 + 1);
+  addNum(random);
+};
+const addNum = function (x) {
+  const newNum = document.querySelectorAll(".casella");
+  for (let i = 1; i < newNum.length; i++)
+    if (parseInt(newNum[i].dataset.numero) === x) {
+      newNum[i].classList.add("selezionato");
+    }
+};
+const button1 = document.querySelector(".estrazione");
+button1.addEventListener("click", numRand);
+TableFunction();
